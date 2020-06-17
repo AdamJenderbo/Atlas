33844e84037a500e012a018f03f865aed5a203b4
Adam Jenderbo <adam.jenderbo@gmail.com>
2020-06-17 16:33:02 +0200

    Key and Mouse Codes

--- Atlas/src/Atlas.h
+++ Atlas/src/Atlas.h
@@ -3,6 +3,11 @@
 #include "Atlas/Application.h"
 #include "Atlas/Log.h"
 #include "Atlas/Layer.h"
+
+#include "Atlas/Input.h"
+#include "Atlas/KeyCodes.h"
+#include "Atlas/MouseButtonCodes.h"
+
 #include "Atlas/ImGui/ImGuiLayer.h"
 
 #include "Atlas/EntryPoint.h"
--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,11 +1,39 @@
 #include <Atlas.h>
 
+class ExampleLayer : public Atlas::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example")
+	{
+	}
+
+	void OnUpdate() override
+	{
+		if (Atlas::Input::IsKeyPressed(KEY_TAB))
+			LOG_INFO("Tab key is pressed (poll)!");
+	}
+
+	void OnEvent(Atlas::Event& event) override
+	{
+		if (event.GetEventType() == Atlas::EventType::KeyPressed)
+		{
+			Atlas::KeyPressedEvent& e = (Atlas::KeyPressedEvent&)event;
+			if (e.GetKeyCode() == KEY_TAB)
+				LOG_INFO("Tab key is pressed (event)!");
+			LOG_INFO("{0}", (char)e.GetKeyCode());
+		}
+	}
+};
+
+
 class Sandbox : public Atlas::Application
 {
 public:
 	Sandbox()
 	{
-		PushLayer(new Atlas::ImGuiLayer());
+		PushLayer(new ExampleLayer());
+		PushOverlay(new Atlas::ImGuiLayer());
 	}
 
 	~Sandbox()
