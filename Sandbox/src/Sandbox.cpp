61464c38a6b0f7cd4e4b74dd6f1baf0fcf713b88
Adam Jenderbo <adam.jenderbo@gmail.com>
2020-06-22 09:46:23 +0200

    Added transforms to renderer

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,6 +1,8 @@
 #include <Atlas.h>
 
 #include "imgui/imgui.h"
+#include <glm/gtc/matrix_transform.hpp>
+
 
 class ExampleLayer : public Atlas::Layer
 {
@@ -34,10 +36,10 @@ public:
 		squareVA.reset(Atlas::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<Atlas::VertexBuffer> squareVB;
@@ -58,13 +60,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 			layout(location = 1) in vec4 a_Color;
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 			out vec3 v_Position;
 			out vec4 v_Color;
 			void main()
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -88,11 +91,13 @@ public:
 			
 			layout(location = 0) in vec3 a_Position;
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
 			out vec3 v_Position;
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -137,7 +142,18 @@ public:
 
 		Atlas::Renderer::BeginScene(m_Camera);
 
-		Atlas::Renderer::Submit(blueShader, squareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Atlas::Renderer::Submit(blueShader, squareVA, transform);
+			}
+		}
+
 		Atlas::Renderer::Submit(shader, vertexArray);
 
 		Atlas::Renderer::EndScene();
